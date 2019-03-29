3779f2d50dbdebbb754119ef41e5d0ad6536d9ac
gonzalormonja <48839385+gonzalormonja@users.noreply.github.com>
2019-03-29 18:55:51 -0300

    nico te cambie el ultimo for, en vez de numeros.lenght, usa la variable espacioenarray

--- workspaceClase/EjerciciosClase/src/tarea3/Numeros.java
+++ workspaceClase/EjerciciosClase/src/tarea3/Numeros.java
@@ -31,7 +31,7 @@ public class Numeros {
 		
 			}
 		Arrays.sort(numeros);
-		for(int i=0;i<numeros.length;i++){
+		for(int i=0;i<=espacioEnArray;i++){
 			
 			if(numeros[i] != 0){
 			System.out.println(numeros[i]);
